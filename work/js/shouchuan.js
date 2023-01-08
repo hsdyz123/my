dd4367eec44ffd27c4e8061e4803661f04af69fc
admin <admin@qq.com>
2023-01-08 08:57:21 +0800

    增加了手传6小时

--- work/js/shouchuan.js
+++ work/js/shouchuan.js
@@ -30,7 +30,22 @@ let arr_shouchuan = [
     let sc_4 = Math.max(arr_sc[1], arr_sc[2], arr_sc[3]).toFixed(1)
     $('#sc4').text(sc_4)
   },
-  // 8h
+  // 6h
+  function sc_6h() {
+    let arr_sc = [
+      NaN,
+      (2.0 + Math.random() * 2).toFixed(1),
+      (2.0 + Math.random() * 2).toFixed(1),
+      (2.0 + Math.random() * 2).toFixed(1),
+    ]
+    for (let i = 1; i <= 3; i++) {
+      $('#sc' + i).text(arr_sc[i])
+    }
+    let sc_4 = (
+      Math.max(arr_sc[1], arr_sc[2], arr_sc[3]) * Math.sqrt(1.5)
+    ).toFixed(1)
+    $('#sc4').text(sc_4)
+  }, // 8h
   function sc_8h() {
     let arr_sc = [
       NaN,
@@ -54,6 +69,9 @@ $('#btn_shouchuan_2h').on('click', function () {
 $('#btn_shouchuan_4h').on('click', function () {
   arr_shouchuan[2]()
 })
-$('#btn_shouchuan_8h').on('click', function () {
+$('#btn_shouchuan_6h').on('click', function () {
   arr_shouchuan[3]()
+})
+$('#btn_shouchuan_8h').on('click', function () {
+  arr_shouchuan[4]()
 })
