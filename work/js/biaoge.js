19de2762d64b12ba92ec2cb3d86c2d922de046df
admin <admin@qq.com>
2023-05-01 15:57:57 +0800

    修改work文件

--- work/js/biaoge.js
+++
@@ -1,9 +0,0 @@
-$('#btn_online').on('click', function () {
-  window.location.href = './files/危害因素.htm'
-})
-$('#btn_downloadXLSX').on('click', function () {
-  window.location.href = './files/危害因素.xlsx'
-})
-$('#btn_downloadPDF').on('click', function () {
-  window.location.href = './files/危害因素.pdf'
-})
